5e678e8edbcfb9ed390760155261c5baf2e76fdc
myshitian <1047221702@qq,com>
2019-03-21 20:45:38 +0800

    'xiugai'

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,15 +1,13 @@
 <template>
-  <swiper :options="swiperOption">
+  <div class="wrapper">
+    <swiper :options="swiperOption">
     <!-- slides -->
 
-    <swiper-slide>
-      <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193
-/2f5f3ea4698c9b7898db7562d89b91ed.jpg_750x200_bd3b4ce9.jpg"/>
-    </swiper-slide>
-    <swiper-slide>
-      <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/
-892f011c3b7bd6815b460c659acc5651.jpg_750x200_e2242cfb.jpg">
-    </swiper-slide>
+     <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img"  :src="item.imgUrl"
+      />
+      </swiper-slide>
+
 
 
     <!-- Optional controls -->
@@ -20,7 +18,7 @@
     <div class="swiper-scrollbar"   slot="scrollbar"></div>
     -->
   </swiper>
-
+  </div>
 </template>
 
 <script>
@@ -30,9 +28,22 @@
     export default {
       components: {SwiperSlide},
       name: "HomeSwiper",
-        data(){
+        data (){
           return{
-            swiperOption:{}
+            swiperOption:{
+              pagination:'.swiper-pagination',
+              loop:true //让轮播插件支持循环轮播
+
+            },
+            swiperList:[{
+              id:'0001',
+              imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193\n' +
+              '/2f5f3ea4698c9b7898db7562d89b91ed.jpg_750x200_bd3b4ce9.jpg'
+            },{
+              id:'0002',
+              imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/\n' +
+              '892f011c3b7bd6815b460c659acc5651.jpg_750x200_e2242cfb.jpg'
+            }]
           }
         }
 
@@ -40,7 +51,15 @@
 </script>
 
 <style lang="stylus" scoped>
-  .swiper-img
+  .wrapper >>> .swiper-pagination-bullet-active
+    background :#ffffff
+  .wrapper
+    overflow :hidden
     width :100%
+    height :0;
+    padding-bottom :26.66%
+    background :#eee
+    .swiper-img
+      width :100%
 
 </style>
