9b55ba03699f70ffe401507e116a34ed85f86b54
myshitian <1047221702@qq,com>
2019-03-21 16:40:56 +0800

    'xiugai'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <home-header></home-header>
+      <home-swiper></home-swiper>
+    </div>
+
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
+
+    export default {
+      components: {
+        HomeSwiper,
+        HomeHeader},
+      name: "Home",
+
+        component: {
+          HomeHeader,
+          HomeSwiper
+        }
+    }
+</script>
+
+<style >
+
+</style>
